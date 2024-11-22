48a9e323077cce56902f2a4fb8cd081f34bfb1d3
SudoCode76 <bo3321319@gmail.com>
2024-11-22 00:39:18 -0400

    correcion en conexion con la base de datos

--- config/database.js
+++ config/database.js
@@ -28,8 +28,8 @@ module.exports = ({ env }) => {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'ecommerceCosmeticos'),
-        user: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
